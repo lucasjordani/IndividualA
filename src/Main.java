0a5c9181f9f9f75d0a3c49c0a1938ebbf6fc35af
othman853 <anuar.yasser@gmail.com>
2015-01-29 15:08:02 -0200

    Primeiro commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		int resposta;
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("O seu animal pode voar?");
+		System.out.println("1 para Sim ou 0 Não");
+		resposta = scan.nextInt();
+		
+		if (resposta == 1) {
+			System.out.println("Será que ele tem penas?");
+			System.out.println("1 para Sim ou 0 Não");
+			resposta = scan.nextInt();
+			if (resposta == 1) {
+				System.out.println("Será que começa com a letra 'A'?");
+				System.out.println("1 para Sim ou 0 Não");
+				resposta = scan.nextInt();
+				if (resposta == 1)
+					System.out.println("O animal que você está pensando é a Andorinha");
+				else
+					System.out.println("O animal que você está pensando é o Papagaio");	
+			} else
+				System.out.println("O animal que você está pensando é o Pavão");;
+		} else {
+			System.out.println("Será que, vive na água?");
+			System.out.println("1 para Sim ou 0 Não");
+			resposta = scan.nextInt();
+			if (resposta == 1){
+				System.out.println("Será que começa com um 'B'?");
+				System.out.println("1 para Sim ou 0 Não");
+				resposta = scan.nextInt();
+				if (resposta == 1)
+					System.out.println("O animal que você está pensando é a Baleia");
+				else
+					System.out.println("O animal que você está pensando é o Peixe");
+			} else {
+				System.out.println("Será que ele rasteja?");
+				System.out.println("1 para Sim ou 0 Não");
+				resposta = scan.nextInt();
+					if (resposta == 1){
+						System.out.println("Será que começa com um 'L'?");
+						System.out.println("1 para Sim ou 0 Não");
+						resposta = scan.nextInt();
+						if (resposta == 1)
+							System.out.println("O animal que você está pensando é o Lagarto");
+						else
+							System.out.println("O animal que você está pensando é o Camaleão");
+						} else {
+							System.out.println("Ele cava buracos?");
+							System.out.println("1 para Sim ou 0 Não");
+							resposta = scan.nextInt();
+						} if (resposta == 1)
+							System.out.println("O animal que você está pensando é o Urso");
+						else
+							System.out.println("O animal que você está pensando é o Macaco");
+				}
+			}
+		}
+			
+	}
